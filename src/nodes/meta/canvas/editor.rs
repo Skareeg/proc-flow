458aa6453307619ce8183afc318e2c66bbf4dd05
Skareeg <skareeg@gmail.com>
2020-07-15 18:59:55 -0400

    Integrated example WGPU Iced.

--- src/nodes/meta/canvas/editor.rs
+++ src/nodes/meta/canvas/editor.rs
@@ -106,3 +106,104 @@ pub fn run_canvas_editor() {
         }
     });
 }
+
+pub struct Scene {
+    pipeline: wgpu::RenderPipeline,
+}
+
+impl Scene {
+    pub fn new(device: &wgpu::Device) -> Scene {
+        let pipeline = build_pipeline(device);
+
+        Scene { pipeline }
+    }
+
+    pub fn clear<'a>(
+        &self,
+        target: &'a wgpu::TextureView,
+        encoder: &'a mut wgpu::CommandEncoder,
+        background_color: iced_winit::Color,
+    ) -> wgpu::RenderPass<'a> {
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: target,
+                resolve_target: None,
+                load_op: wgpu::LoadOp::Clear,
+                store_op: wgpu::StoreOp::Store,
+                clear_color: {
+                    let [r, g, b, a] = background_color.into_linear();
+
+                    wgpu::Color {
+                        r: r as f64,
+                        g: g as f64,
+                        b: b as f64,
+                        a: a as f64,
+                    }
+                },
+            }],
+            depth_stencil_attachment: None,
+        })
+    }
+
+    pub fn draw<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
+        render_pass.set_pipeline(&self.pipeline);
+        render_pass.draw(0..3, 0..1);
+    }
+}
+
+fn build_pipeline(device: &wgpu::Device) -> wgpu::RenderPipeline {
+    let vs = include_bytes!("shader/vert.spv");
+    let fs = include_bytes!("shader/frag.spv");
+
+    let vs_module = device.create_shader_module(
+        &wgpu::read_spirv(std::io::Cursor::new(&vs[..])).unwrap(),
+    );
+
+    let fs_module = device.create_shader_module(
+        &wgpu::read_spirv(std::io::Cursor::new(&fs[..])).unwrap(),
+    );
+
+    let pipeline_layout =
+        device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            bind_group_layouts: &[],
+        });
+
+    let pipeline =
+        device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            layout: &pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::None,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
+            depth_stencil_state: None,
+            // vertex_state: wgpu::VertexStateDescriptor {
+            //     index_format: wgpu::IndexFormat::Uint16,
+            //     vertex_buffers: &[],
+            // },
+            index_format: wgpu::IndexFormat::Uint16,
+            vertex_buffers: &[],
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+    pipeline
+}
